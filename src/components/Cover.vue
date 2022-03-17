9938edd7989b15417c4b9d1f5c931e71459f340d
Pony <ponyma2100@gmail.com>
2022-03-17 23:23:10 +0800

    Project init

---
+++ src/components/Cover.vue
@@ -0,0 +1,63 @@
+<template>
+  <header>
+    <div class="container-left">
+      <div class="title">
+        <h1>More than just shorter links</h1>
+      </div>
+      <div class="subtitle">
+        <h4>
+          Build your brand's recognition and get detailed insights on how your
+          links are performing.
+        </h4>
+      </div>
+      <div class="start">
+        <button>Get Start</button>
+      </div>
+    </div>
+    <div class="container-right">
+      <img src="../assets/working.svg" alt="" />
+    </div>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  height: 500px;
+}
+.title {
+  font-size: 36px;
+  font-weight: bold;
+  padding: 0;
+}
+.subtitle {
+  padding: 0;
+  color: hsl(0, 0%, 75%);
+}
+.container-left {
+  width: 60%;
+  display: flex;
+  flex-direction: column;
+  text-align: start;
+}
+
+.start button {
+  width: 130px;
+  height: 40px;
+}
+
+.container-right {
+  width: 40%;
+  overflow: hidden;
+}
+
+.container-right img {
+  width: 40vw;
+  min-width: 20vw;
+  right: 60px;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="navbar">
+    <nav>
+      <div class="logo">
+        <img src="../assets/logo.svg" alt="" />
+      </div>
+      <div class="guide">
+        <ul>
+          <li><a href="#">Features</a></li>
+          <li><a href="#">Pricing</a></li>
+          <li><a href="#">Resouces</a></li>
+        </ul>
+      </div>
+      <div class="links">
+        <div class="login"><a href="#">Login</a></div>
+        <!-- <button class="login">Login</button> -->
+        <button class="signup">Sign Up</button>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.navbar {
+  padding: 16px 10px;
+  background: white;
+  padding: 0 20px;
+}
+nav {
+  display: flex;
+  align-items: center;
+  margin: 0 auto;
+}
+
+nav ul {
+  display: flex;
+  list-style-type: none;
+  font-size: 15px;
+  font-weight: bold;
+}
+nav .links {
+  display: flex;
+  margin-left: auto;
+  font-size: 15px;
+}
+</style>
